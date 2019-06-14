20e18e7d15a9735df5d30071e4c0bbd48f7a2339
willamylp <willamy.wlp@gmail.com>
2019-06-13 21:47:36 -0300

    Ajustes nos Arquivos do Projeto

--- Agenda/views.py
+++ Agenda/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def ListarContatos(request):
+    contatos = Hosts.objects.all().values()
+    contatos2 = json.loads(json.dumps(list(contatos), cls=DjangoJSONEncoder))
+    for i in range(len(contatos2)):
+        print("-->>>> Host {}: {}".format(i, contatos2[i]['hostname']))
+        print(contatos2[i].values())
+    return render(request, 'listagemContatos/ListarContatos.html', {'contatos': contatos})
+# Create your views here.
